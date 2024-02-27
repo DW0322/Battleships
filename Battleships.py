95343f104b4181deef6996be4af5c3a702dbf8c4
DanielEvans22 <dwevans2203@gmail.com>
2024-02-27 00:03:57 +0000

    New features

--- Battleships.py
+++ Battleships.py
@@ -1,6 +1,4 @@
-#battle-ship board design
-#two board for each player
-#one to place the ships, one to shoot the ships
+#battleship board layout. Seperated into lists for appending placement of boats and attacks.
 boardShipP1 = ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", 
                "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", 
                "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", 
@@ -47,10 +45,10 @@ winner = None
 gameRunning = True
 currentPlayer = "P1"
 
-#define board print functions
+#function to print the esablished layout in a more user friendly way. Also declaring the rows and columns.
 def print_board(player_number, attack_board, ship_board):
     print("                 ATTACK BOARD")
-    print("   ", "A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J ")
+    print("    ", "A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J ")
     for i in range(10):
         print("{:2d}".format(i + 1), end=" |")
         for j in range(10):
@@ -58,17 +56,55 @@ def print_board(player_number, attack_board, ship_board):
         print()
 
     print("                 SHIP BOARD")
-    print("   ", "A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J ")
+    print("    ", "A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J ")
     for i in range(10):
         print("{:2d}".format(i + 1), end=" |")
         for j in range(10):
             print(" " + ship_board[i * 10 + j] + " |", end="")
         print()
-#battle ship design for vertical and horizontal placement
-#battle ship placement round player 1
 
-print_board(currentPlayer, boardShipP1, boardAttackP1)
+print_board(currentPlayer, boardAttackP1, boardShipP1)
 
+#Fucniton checks if you've already selected that boat and if the are you are selecting has enough space for the ship.
+def boardShipPlacement(size, ship_board, cell_selected, boat_symbol, direction):
+    directions = {
+        "W": (-1, 0),  # Up
+        "A": (0, -1),  # Left
+        "S": (1, 0),   # Down
+        "D": (0, 1)    # Right
+    }
+
+    num_columns = 10
+    num_rows = 10
+
+    if direction in directions:
+        row_step, col_step = directions[direction]
+        indexes_to_check = [cell_selected]
+
+        for i in range(1, size):
+            next_row = (cell_selected // num_columns) + (row_step * i)
+            next_col = (cell_selected % num_columns) + (col_step * i)
+            next_index = next_row * num_columns + next_col
+
+            # Check if the next index is within the grid boundaries
+            if 0 <= next_row < num_rows and 0 <= next_col < num_columns:
+                indexes_to_check.append(next_index)
+            else:
+                return False, "Ship placement out of bounds"
+
+        # Check if all subsequent indexes are available
+        if all(ship_board[index] == "-" for index in indexes_to_check):
+            # Place the ship on the board
+            for index in indexes_to_check:
+                ship_board[index] = boat_symbol
+            return True, "Ship placed successfully"
+        else:
+            return False, "Some cells are already occupied"
+    else:
+        return False, "Invalid direction"
+
+            
+#Function to take user input for the placement of the 5 types of ships. 
 def boatPlacement():
     
     boats = {
@@ -85,17 +121,19 @@ def boatPlacement():
     remaining_boats = set(boats.keys())
 
     selected_boats = []
-
+    
  
     while remaining_boats:
         for boat_id, boat_text in boatsText.items():
             if boat_id in remaining_boats:
                 print(boat_text)
+
         boat = input("Pick the number boat you would like to place: ")
 
         if boat in remaining_boats:
+            boat_symbol = boats[boat][0]
             size = boats[boat][1]
-            selected_boats.append((boats[boat][0], boats[boat][1]))
+            selected_boats.append((boat_symbol, size))
             remaining_boats.remove(boat)
         else:
             print("Invalid choice. Please pick a boat from the list.")
@@ -104,42 +142,48 @@ def boatPlacement():
         placement = []
 
         column = set(column_values.keys())
-
-        while True:
-            placement_column = (input(f"What column (A - J) would you like to place your {boats[boat][2]} of size {size}? ")).upper()
-            placement.append(placement_column)
-            if placement_column in column:
-                break
-            else:
-                print("Invalid column. Please enter a column from A to J.")
-
         row = set(row_values.keys())
 
         while True:
-            placement_row = (input(f"What row (1 - 10) would you like to place your {boats[boat][2]} of size {size}? ")).upper()
-            placement.append(placement_row)
-            if placement_row in row:
-                break
-            else:
-                print("Invalid row. Please enter a row from 1 to 10.")
+            
+            while True:
+                placement_column = (input(f"What column (A - J) would you like to place your {boats[boat][2]} of size {size}? ")).upper()
+                if placement_column in column:
+                    placement.append(placement_column)
+                    break
+                else:
+                    print("Invalid column. Please enter a column from A to J.")
+                    continue
+
+            while True:
+                placement_row = (input(f"What row (1 - 10) would you like to place your {boats[boat][2]} of size {size}? ")).upper()
+                if placement_row in row:
+                    placement.append(placement_row)
+                    break
+                else:
+                    print("Invalid row. Please enter a row from 1 to 10.")
+                    continue
+            break
+
         column_selected = column_values[placement[0]]
         row_selected = row_values[placement[1]]
         cell_selected = column_selected + row_selected
+            
 
-        #def boardShipPlacement(player_number, ship_board, selected_cell):
-            #if cell_selected == "-" and column
-
+    
         available_directions = ["W", "A", "S", "D"]
-
         while True:
             direction = (input("Using the WASD keys, select your desired direction: ")).upper()
             if direction in available_directions:
-                break
+                success, message = boardShipPlacement(size, boardShipP1, cell_selected, boat_symbol, direction)
+                if success:
+                    print_board(currentPlayer, boardAttackP1, boardShipP1)
+                    break
+                else:
+                    print(message)
             else:
                 print("Invalid direction. Please enter a direction using WASD keys.")
         
-
-    return print(selected_boats)
 
 boatPlacement()
 
